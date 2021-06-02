1512da51ca31d91390b51b5e07a1f1f9761c88aa
TejasPatel <tjp083@gmail.com>
2021-06-02 12:15:40 -0400

    done

---
+++ myScript.js
@@ -0,0 +1,134 @@
+const container1 = document.querySelector('.rating1');
+    const items1 = container1.querySelectorAll('.rating-item1')
+    container1.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items1.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate1"));
+        elClass.add('active');
+      }
+    };
+  
+    const container2 = document.querySelector('.rating2');
+    const items2 = container2.querySelectorAll('.rating-item2')
+    container2.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items2.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate2"));
+        elClass.add('active');
+      }
+    };
+
+    const container3 = document.querySelector('.rating3');
+    const items3 = container3.querySelectorAll('.rating-item3')
+    container3.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items3.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate3"));
+        elClass.add('active');
+      }
+    };
+ 
+    const container4 = document.querySelector('.rating4');
+    const items4 = container4.querySelectorAll('.rating-item4')
+    container4.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items4.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate4"));
+        elClass.add('active');
+      }
+    };
+  
+    const container5 = document.querySelector('.rating5');
+    const items5 = container5.querySelectorAll('.rating-item5')
+    container5.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items5.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate5"));
+        elClass.add('active');
+      }
+    };
+  
+    const container6 = document.querySelector('.rating6');
+    const items6 = container6.querySelectorAll('.rating-item6')
+    container6.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items6.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate6"));
+        elClass.add('active');
+      }
+    };
+  
+    const container7 = document.querySelector('.rating7');
+    const items7 = container7.querySelectorAll('.rating-item7')
+    container7.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items7.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate7"));
+        elClass.add('active');
+      }
+    };
+  
+    const container8 = document.querySelector('.rating8');
+    const items8 = container8.querySelectorAll('.rating-item8')
+    container8.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items8.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate8"));
+        elClass.add('active');
+      }
+    };
+  
+    const container9 = document.querySelector('.rating9');
+    const items9 = container9.querySelectorAll('.rating-item9')
+    container9.onclick = e => {
+      const elClass = e.target.classList;
+
+      if (!elClass.contains('active')) {
+        items9.forEach(
+          item => item.classList.remove('active'));
+
+        
+        console.log(e.target.getAttribute("data-rate9"));
+        elClass.add('active');
+      }
+    };
